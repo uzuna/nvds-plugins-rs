67f80c946d782a1ce6552ba4e13b44271e468219
Fumiya Fujinaka <uzuna.kf@gmail.com>
2022-08-02 17:33:31 +0900

    add comment for properties

--- scripts/single_image_infer.sh
+++ scripts/single_image_infer.sh
@@ -1,10 +1,15 @@
+if [ $# -ne 1 ]; then
+  echo "must set <config-file-path>" 1>&2
+  exit 1
+fi
+
 gst-launch-1.0 \
 v4l2src device=/dev/video0 num-buffers=10 ! \
 decodebin ! \
 videoconvert ! \
 nvvideoconvert ! \
 m.sink_0 nvstreammux name=m batch-size=1 width=1280 height=720 batched-push-timeout=40000 nvbuf-memory-type=0 ! \
-nvinfer config-file-path=config_infer_yolov3.txt ! \
+nvinfer config-file-path=$1 ! \
 queue ! \
 nvdsosd ! \
 nvstreamdemux name=d d.src_0 ! fakesink
